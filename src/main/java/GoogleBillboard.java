9a3ae6aa956ff9517db6f61c5abf596e3e805b6a
RonanW123 <142534416+RonanW123@users.noreply.github.com>
2024-01-16 10:43:43 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -9,6 +9,12 @@ public class GoogleBillboard {
 
 	//Finish this function
 	public boolean isPrime(){
+		for(int i = 2; i <= Math.sqrt(num); i++)
+ 			if(num%i == 0)
+      				return false;
+  			if(num < 2)
+    				return false;
+  		return true;
 	}
 
 }
