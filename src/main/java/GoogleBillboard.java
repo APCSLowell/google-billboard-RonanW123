fdf41798f200fdfb903e20192639f058496068a6
RonanW123 <142534416+RonanW123@users.noreply.github.com>
2024-01-16 16:56:48 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -10,10 +10,10 @@ public class GoogleBillboard {
 	//Finish this function
 	public boolean isPrime(){
 		for(int i = 0; i < e.length(); i++){
-			String tenNumsS = new String(e.substring(i, i+10));
+			String tenNumsS = e.substring(i, i+10);
 			double tenNumsD = Double.parseDouble(tenNumsS);
-			for(int i = 2; i <= Math.sqrt(tenNumsD); i++)
- 				if(tenNumsD%i == 0)
+			for(int x = 2; x <= Math.sqrt(tenNumsD); x++)
+ 				if(tenNumsD%x == 0)
       					return false;
   				if(tenNumsD < 2)
     					return false;
