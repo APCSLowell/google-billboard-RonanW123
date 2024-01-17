8fa7e8089f89471ed921afa64c55a00be1e83dc3
RonanW123 <142534416+RonanW123@users.noreply.github.com>
2024-01-16 22:26:10 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,25 +1,28 @@
-
-
 public class GoogleBillboard {
-	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
-	}
-	
-
-	//Finish this function
-	public boolean isPrime(){
-		for(int i = 0; i < e.length()-10; i++){
-			String tenNumsS = e.substring(i, i+10);
-			double tenNumsD = Double.parseDouble(tenNumsS);
-			for(int x = 2; x <= Math.sqrt(tenNumsD); x++)
- 				if(tenNumsD%x == 0)
-      					return false;
-  				if(tenNumsD < 2)
-    					return false
-		}
-  		return true;
-	}
+  public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+  public static void main(final String[] args) {
+    System.out.println("Not the right string, tests will fail!");
+    System.out.println(setup());
+  }
+  
+  public String setup(){
+     for(int i = 2; i < e.length()-12; i++){
+        String tenNumsS = e.substring(i, i+10);
+        double tenNumsD = Double.parseDouble(tenNumsS);
+          if(isPrime(tenNumsD))
+            return tenNumsD + "";
+     }
+     return "";
+  }
+  //Finish this function
+  public boolean isPrime(double dNum){
+      for(int i = 2; i <= Math.sqrt(dNum); i++)
+          if(dNum%i == 0)
+              return false;
+           if(dNum < 2)
+              return false;
+      return true; 
+  }
 
 }
 
