7d15ebedfab226cb9a8dcbcc2c6b3c58ae14fe7c
RonanW123 <142534416+RonanW123@users.noreply.github.com>
2024-01-16 22:30:55 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,18 +1,17 @@
 public class GoogleBillboard {
   public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
   public static void main(final String[] args) {
-    System.out.println(setup());
-  }
-  
-  public String setup(){
      for(int i = 2; i < e.length()-12; i++){
         String tenNumsS = e.substring(i, i+10);
         double tenNumsD = Double.parseDouble(tenNumsS);
-          if(isPrime(tenNumsD))
-            return tenNumsD + "";
+          if(isPrime(tenNumsD)){
+            System.out.println(tenNumsD);
+            break;
+          }
      }
-     return "";
+     return;
   }
+
   //Finish this function
   public boolean isPrime(double dNum){
       for(int i = 2; i <= Math.sqrt(dNum); i++)
